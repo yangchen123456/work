7e0b3dd190b2c11d430e51bbf7b755b9d3355c6c
yangchen123456 <909657087@qq.com>
2021-01-13 21:09:00 +0800

    work

--- src/com/test/work/work.java
+++ src/com/test/work/work.java
@@ -4,7 +4,27 @@ import java.io.UnsupportedEncodingException;
 
 public class work {
     public static void main(String[] args) {
+        //定义中文字符
         String name = "杨公辰";
+
+        String uname = "";
+        uname += "[";
+        // 将name转化为数组
+        char[] chars = name.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (i == chars.length - 1) {
+                uname += chars[i];
+            }else {
+                uname += chars[i];
+                uname += ",";
+            }
+        }
+        uname += "]";
+        
+        char[] chars1 = uname.toCharArray();
+        System.out.println(chars1);
+    }
     }
 
-}
+
